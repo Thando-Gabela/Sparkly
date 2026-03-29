67f35d653e54dd509bcc40ba7d9dc7be6bcd24bf
Thando-Gabela <ST10522952@myemeris.edu.za>
2026-03-29 21:37:46 +0200

    "Build: Added Reset button to UI and initialized all lateinit variables. 🎀 Setting up for final logic implementation. ✨"

--- app/src/main/java/com/example/sparkly/MainActivity.kt
+++ app/src/main/java/com/example/sparkly/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.sparkly
 
+import android.adservices.ondevicepersonalization.ExecuteInput
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //declarations
+
+    private lateinit var etTimeInput: EditText
+    private lateinit var btnGetSpark: Button
+    private lateinit var btnReset: Button
+    private lateinit var tvresultDisplay: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+
+        etTimeInput=findViewById(R.id.etTimeInput)
+        btnGetSpark=findViewById(R.id.btnGetSpark)
+        btnReset=findViewById(R.id.btnReset)
+        tvresultDisplay=findViewById(R.id.tvResultDisplay)
+
+
+
+
+
+
+
+           }
+
+
+
+       }
+
+
+
